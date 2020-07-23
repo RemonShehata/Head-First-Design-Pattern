ea69521cb450c11d171525da99b2bcef6b8b7198
RemonShehata <RemonGerges94@gmail.com>
2020-07-24 01:01:07 +0200

    Ran the code from main

--- SimUDuck/src/Main.java
+++ SimUDuck/src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
+
+    private static Duck duck;
+
     public static void main(String[] args) {
-        
+        Main m = new Main();
+
+        duck = new MallardDuck();
+        m.callDuckMethods();
+
+        duck = new RedheadDuck();
+        m.callDuckMethods();
+
+        duck = new RubberDuck();
+        m.callDuckMethods();
+    }
+
+    private void callDuckMethods() {
+        duck.display();
+        duck.quack();
+        duck.swim();
+        duck.fly();
+        System.out.println("**************");
     }
 }
