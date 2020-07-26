fed2a6e909b1a55245bd548b3e707d5fd7ac551e
RemonShehata <RemonGerges94@gmail.com>
2020-07-26 16:49:26 +0200

    Created WeatherStation With Built in Observer Implemented one Display

---
+++ Observer/Weather BuiltIn Observer/src/WeatherStation.java
@@ -0,0 +1,14 @@
+import java.util.Observable;
+
+public class WeatherStation {
+    public static void main(String[] args) {
+        Observable o = new WeatherData();
+        
+        CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(o);
+
+        WeatherData weatherData = (WeatherData) o;
+        weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(82, 70, 29.2f);
+        weatherData.setMeasurements(78, 90, 29.2f);
+    }
+}
